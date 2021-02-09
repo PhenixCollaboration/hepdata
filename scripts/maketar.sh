ed9aaf31668be30119c29c91b62579f0b341b6f4
Maxim Potekhin <potekhin@bnl.gov>
2021-02-09 13:12:24 -0500

    Updated the tar script to automate naming of the tar and detection of PNGs

--- scripts/maketar.sh
+++ scripts/maketar.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-echo Making a tar file with the name $1.tar
-tar -cvf $1.tar *.yaml *.png
+name=''
+
+if [ -z "$1" ]
+then
+    name=${PWD##*/}
+else
+    name=$1
+fi
+
+if [ -f "submission.yaml" ]
+then
+    echo Making a tar file for HEPData: $name.tar
+    echo ---------------------------------------------------------
+else
+    echo The file submission.yaml not found, exiting
+    exit -1
+fi
+
+if ls *.png 1> /dev/null 2>&1
+then
+    tar -cvf $name.tar *.yaml *.png
+else
+    tar -cvf $name.tar *.yaml
+fi
+
